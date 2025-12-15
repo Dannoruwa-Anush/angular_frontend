3b22c77e9458bb251477f84c6f08ed100937663f
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-15 20:51:55 +0530

    layout is modified

--- src/app/components/reusable_components/layout-component/layout-component.ts
+++ src/app/components/reusable_components/layout-component/layout-component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { HeaderComponent } from '../header-component/header-component';
 import { FooterComponent } from '../footer-component/footer-component';
