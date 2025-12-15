2591b46e333589239a2398940ebde6c7c6e5cecf
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-15 15:29:18 +0530

    api response is modified

--- src/app/services/api_services/core_services/crud-service.ts
+++ src/app/services/api_services/core_services/crud-service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../config/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { ApiResponse } from '../../../models/api_models/core_api_models/apiResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -16,22 +17,32 @@ export abstract class CrudService<T> {
   ) { }
 
   getAll(): Observable<T[]> {
-    return this.http.get<T[]>(`${this.baseUrl}/${this.endpoint}`);
+    return this.http
+      .get<ApiResponse<T[]>>(`${this.baseUrl}/${this.endpoint}`)
+      .pipe(map(res => res.data));
   }
 
   getById(id: number | string): Observable<T> {
-    return this.http.get<T>(`${this.baseUrl}/${this.endpoint}/${id}`);
+    return this.http
+      .get<ApiResponse<T>>(`${this.baseUrl}/${this.endpoint}/${id}`)
+      .pipe(map(res => res.data));
   }
 
   create(data: T): Observable<T> {
-    return this.http.post<T>(`${this.baseUrl}/${this.endpoint}`, data);
+    return this.http
+      .post<ApiResponse<T>>(`${this.baseUrl}/${this.endpoint}`, data)
+      .pipe(map(res => res.data));
   }
 
   update(id: number | string, data: T): Observable<T> {
-    return this.http.put<T>(`${this.baseUrl}/${this.endpoint}/${id}`, data);
+    return this.http
+      .put<ApiResponse<T>>(`${this.baseUrl}/${this.endpoint}/${id}`, data)
+      .pipe(map(res => res.data));
   }
 
   delete(id: number | string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${this.endpoint}/${id}`);
+    return this.http
+      .delete<ApiResponse<void>>(`${this.baseUrl}/${this.endpoint}/${id}`)
+      .pipe(map(() => void 0));
   }
 }
