fd937f425ffd23ad4118507ca6ed7abdce2adb48
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-16 06:52:28 +0530

    product component is added to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/reusable_components/layout-component/layout-component';
 import { HomeComponent } from './components/page_components/home-component/home-component';
 import { ProductsComponent } from './components/page_components/products-component/products-component';
+import { ProductComponent } from './components/page_components/product-component/product-component';
 
 export const routes: Routes = [
     {
@@ -11,7 +12,8 @@ export const routes: Routes = [
         //nested routes
         children: [
             { path: '', component: HomeComponent },
-            { path: 'products', component: ProductsComponent }
+            { path: 'products', component: ProductsComponent },
+            { path: 'product/:id', component: ProductComponent },
         ]
     }
 ];
