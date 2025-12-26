2f9e0e27db8c8fc49a8487e8cfee252131a68d8b
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-26 20:10:59 +0530

    praduct nav is drafted

--- src/app/components/page_components/dashboard_components/nav_components/product-nav-component/product-nav-component.ts
+++ src/app/components/page_components/dashboard_components/nav_components/product-nav-component/product-nav-component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardNavStateBase } from '../../../../reusable_components/dashboard_nav_component/dashboardNavStateBase';
+import { ElectronicItemModel } from '../../../../../models/api_models/electronicItemModel';
+import { DashboardModeEnum } from '../../../../../config/enums/dashboardModeEnum';
+import { ElectronicItemService } from '../../../../../services/api_services/electronicItemService';
+import { FormBuilder } from '@angular/forms';
+import { SystemMessageService } from '../../../../../services/ui_service/systemMessageService';
+import { CrudOperationConfirmationUiHelper } from '../../../../../utils/crudOperationConfirmationUiHelper';
 
 @Component({
   selector: 'app-product-nav-component',
@@ -6,6 +13,37 @@ import { Component } from '@angular/core';
   templateUrl: './product-nav-component.html',
   styleUrl: './product-nav-component.scss',
 })
-export class ProductNavComponent {
+export class ProductNavComponent extends DashboardNavStateBase<ElectronicItemModel> {
 
+
+
+
+  // ======================================================
+  // CONSTRUCTOR
+  // ======================================================
+  constructor(
+    private electronicItemService: ElectronicItemService,
+    private messageService: SystemMessageService,
+    private confirmationHelper: CrudOperationConfirmationUiHelper,
+    private fb: FormBuilder,
+  )
+  {
+    super();
+  }
+  
+  // ======================================================
+  // BASE CLASS IMPLEMENTATIONS
+  // ======================================================
+  protected override getId(item: ElectronicItemModel): number | null {
+    throw new Error('Method not implemented.');
+  }
+  protected override loadItems(): void {
+    throw new Error('Method not implemented.');
+  }
+  protected override loadToForm(item: ElectronicItemModel, mode: DashboardModeEnum): void {
+    throw new Error('Method not implemented.');
+  }
+  protected override resetForm(): void {
+    throw new Error('Method not implemented.');
+  }
 }
