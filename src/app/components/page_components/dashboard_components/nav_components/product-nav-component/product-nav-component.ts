5d70a0292eb8d6238f4dae5ac11be964e7955631
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-26 22:53:31 +0530

    product nav is drafted

--- src/app/components/page_components/dashboard_components/nav_components/product-nav-component/product-nav-component.ts
+++ src/app/components/page_components/dashboard_components/nav_components/product-nav-component/product-nav-component.ts
@@ -1,49 +1,295 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal, ViewChild } from '@angular/core';
 import { DashboardNavStateBase } from '../../../../reusable_components/dashboard_nav_component/dashboardNavStateBase';
 import { ElectronicItemModel } from '../../../../../models/api_models/electronicItemModel';
 import { DashboardModeEnum } from '../../../../../config/enums/dashboardModeEnum';
 import { ElectronicItemService } from '../../../../../services/api_services/electronicItemService';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SystemMessageService } from '../../../../../services/ui_service/systemMessageService';
 import { CrudOperationConfirmationUiHelper } from '../../../../../utils/crudOperationConfirmationUiHelper';
+import { DashboardTableColumnModel } from '../../../../../models/ui_models/dashboardTableColumnModel';
+import { BrandModel } from '../../../../../models/api_models/brandModel';
+import { CategoryModel } from '../../../../../models/api_models/categoryModel';
+import { BrandService } from '../../../../../services/api_services/brandService';
+import { CategoryService } from '../../../../../services/api_services/categoryService';
+import { environment } from '../../../../../config/environment';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../../../../custom_modules/material/material-module';
+import { DashboardFormComponent } from '../../../../reusable_components/dashboard_nav_component/dashboard_building_blocks/dashboard-form-component/dashboard-form-component';
+import { DashboardTableComponent } from '../../../../reusable_components/dashboard_nav_component/dashboard_building_blocks/dashboard-table-component/dashboard-table-component';
 
 @Component({
   selector: 'app-product-nav-component',
-  imports: [],
+  imports: [
+    MaterialModule,
+    ReactiveFormsModule,
+    CommonModule,
+    DashboardFormComponent,
+    DashboardTableComponent
+  ],
   templateUrl: './product-nav-component.html',
   styleUrl: './product-nav-component.scss',
 })
 export class ProductNavComponent extends DashboardNavStateBase<ElectronicItemModel> {
 
 
+  // ---------- COMPONENT SPECIFIC STATE ----------
+  brands = signal<BrandModel[]>([]);
+  categories = signal<CategoryModel[]>([]);
 
+  selectedBrandId = signal<number | undefined>(undefined);
+  selectedCategoryId = signal<number | undefined>(undefined);
+
+  selectedBrandName = computed(() =>
+    this.brands().find(b => b.brandID === this.selectedBrandId())?.brandName
+  );
+
+  selectedCategoryName = computed(() =>
+    this.categories().find(c => c.categoryID === this.selectedCategoryId())?.categoryName
+  );
+
+  private loadBrands(): void {
+    this.brandService.getAll().subscribe(res => {
+      this.brands.set(res);
+    });
+  }
+
+  private loadCategories(): void {
+    this.categoryService.getAll().subscribe(res => {
+      this.categories.set(res);
+    });
+  }
+
+  onBrandSelect(id?: number) {
+    this.pageNumber.set(1);
+    this.selectedBrandId.set(id);
+  }
+
+  onCategorySelect(id?: number) {
+    this.pageNumber.set(1);
+    this.selectedCategoryId.set(id);
+  }
+
+  private baseUrl = environment.BASE_API_URL.replace(/\/$/, '');
+
+  getImageUrl(item: ElectronicItemModel): string {
+    if (item.electronicItemImageUrl) {
+      return item.electronicItemImageUrl;
+    }
+    if (item.electronicItemImage) {
+      return `${this.baseUrl}/${item.electronicItemImage}`;
+    }
+    return 'assets/images/no-image.png';
+  }
+
+  // ======================================================
+  // FORM
+  // ======================================================
+  form!: FormGroup;
+  submitted = false;
+
+  @ViewChild(FormGroupDirective)
+  private formDirective!: FormGroupDirective;
+
+  // ======================================================
+  // TABLE CONFIG
+  // ======================================================
+  columns: DashboardTableColumnModel<ElectronicItemModel>[] = [
+    {
+      key: 'electronicItemName',
+      header: 'Name',
+      cell: e => e.electronicItemName
+    },
+    {
+      key: 'price',
+      header: 'Price (Rs.)',
+      cell: e => e.price
+    },
+    {
+      key: 'qoh',
+      header: 'QOH',
+      cell: e => e.qoh
+    },
+    {
+      key: 'brandName',
+      header: 'Brand',
+      cell: e => e.brandResponseDto!.brandName
+    },
+    {
+      key: 'categoryName',
+      header: 'Category',
+      cell: e => e.categoryResponseDto!.categoryName
+    },
+  ];
+
+  displayedColumns = ['electronicItemName', 'price', 'qoh', 'brandName', 'categoryName'];
 
   // ======================================================
   // CONSTRUCTOR
   // ======================================================
   constructor(
+    private brandService: BrandService,
+    private categoryService: CategoryService,
     private electronicItemService: ElectronicItemService,
     private messageService: SystemMessageService,
     private confirmationHelper: CrudOperationConfirmationUiHelper,
     private fb: FormBuilder,
-  )
-  {
+  ) {
     super();
+
+    this.buildForm();
+    this.loading = this.electronicItemService.loading;
+
+    // Load static data
+    this.loadBrands();
+    this.loadCategories();
+
+    // Auto reload when paging / search changes
+    effect(() => {
+      this.requestParams();
+      this.loadItems();
+    });
   }
-  
+
+  // ======================================================
+  // REACTIVE FORM SETUP
+  // ======================================================
+  private buildForm(): void {
+    this.form = this.fb.group({
+      electronicItemName: ['', [Validators.required, Validators.pattern(/^[A-Za-z\s]+$/)]],
+      price: [''],
+      qoh: [''],
+      imageFile: [''],
+      brandId: [''],
+      categoryId: [''],
+    });
+  }
+
+  // ===============================
+  // GETTERS
+  // ===============================
+  get electronicItemNameCtrl() {
+    return this.form.get('electronicItemName')!;
+  }
+
   // ======================================================
   // BASE CLASS IMPLEMENTATIONS
   // ======================================================
   protected override getId(item: ElectronicItemModel): number | null {
-    throw new Error('Method not implemented.');
+    return item.electronicItemID ?? null;
   }
   protected override loadItems(): void {
-    throw new Error('Method not implemented.');
+    const params = this.requestParams();
+
+    this.electronicItemService
+      .getElectronicItemPaged(
+        params.pageNumber,
+        params.pageSize,
+        1,//this.selectedCategoryId,
+        1,//this.selectedBrandId,
+        params.searchKey
+      )
+      .subscribe(res => {
+        this.items.set(res.items);
+        this.totalCount.set(res.totalCount);
+      });
   }
+
   protected override loadToForm(item: ElectronicItemModel, mode: DashboardModeEnum): void {
-    throw new Error('Method not implemented.');
+    this.selectedItemId.set(item.electronicItemID ?? null);
+
+    this.form.patchValue({
+      electronicItemName: item.electronicItemName
+    });
+
+    mode === DashboardModeEnum.VIEW
+      ? this.form.disable()
+      : this.form.enable();
+
+    this.formMode.set(mode);
   }
   protected override resetForm(): void {
-    throw new Error('Method not implemented.');
+    this.submitted = false;
+    this.formDirective.resetForm();
+
+    this.form.enable();
+    this.selectedItemId.set(null);
+    this.formMode.set(DashboardModeEnum.CREATE);
+  }
+
+  // ===============================
+  // SUBMIT
+  // ===============================
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid || this.isViewMode()) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.isEditMode() ? this.update() : this.save();
+  }
+
+  // ======================================================
+  // CRUD OPERATIONS
+  // ======================================================
+  save(): void {
+    this.confirmationHelper.confirmSave('product').subscribe(confirmed => {
+      if (!confirmed) return;
+
+      const payload: ElectronicItemModel = this.form.getRawValue();
+
+      this.electronicItemService.create(payload).subscribe({
+        next: () => {
+          this.messageService.success('Product saved successfully');
+          this.resetForm();
+          this.loadItems();
+        },
+        error: err => {
+          this.messageService.error(err?.error?.message || 'Save failed');
+        }
+      });
+    });
+  }
+
+  update(): void {
+    const id = this.selectedItemId();
+    if (!id) return;
+
+    this.confirmationHelper.confirmUpdate('product').subscribe(confirmed => {
+      if (!confirmed) return;
+
+      const payload: ElectronicItemModel = {
+        electronicItemID: id,
+        ...this.form.getRawValue()
+      }
+
+      this.electronicItemService.update(payload.electronicItemID!, payload).subscribe({
+        next: () => {
+          this.messageService.success('Product updated successfully');
+          this.resetForm();
+          this.loadItems();
+        },
+        error: err => {
+          this.messageService.error(err?.error?.message || 'Update failed');
+        }
+      });
+    });
+  }
+
+  delete(item: ElectronicItemModel): void {
+    this.confirmationHelper.confirmDelete('product').subscribe(confirmed => {
+      if (!confirmed) return;
+
+      this.electronicItemService.delete(item.electronicItemID!).subscribe({
+        next: () => {
+          this.messageService.success('Product deleted successfully');
+          this.loadItems();
+        },
+        error: err => {
+          this.messageService.error(err?.error?.message || 'Delete failed');
+        }
+      });
+    });
   }
 }
