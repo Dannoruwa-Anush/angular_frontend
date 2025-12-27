9038a0033d30a66f7e1d4a789f655645e562f81c
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-27 10:03:39 +0530

    employee nav component is added

---
+++ src/app/components/page_components/dashboard_components/nav_components/employee-nav-component/employee-nav-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-nav-component',
+  imports: [],
+  templateUrl: './employee-nav-component.html',
+  styleUrl: './employee-nav-component.scss',
+})
+export class EmployeeNavComponent {
+
+}
