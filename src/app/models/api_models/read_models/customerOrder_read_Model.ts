3abdf372aa5b81590bd48e942a0de8a73481dc0f
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-31 13:20:14 +0530

    order summary is modified

--- src/app/models/api_models/read_models/customerOrder_read_Model.ts
+++ src/app/models/api_models/read_models/customerOrder_read_Model.ts
@@ -16,5 +16,5 @@ export interface CustomerOrderReadModel {
 
     //FK: Fields
     customerResponseDto?: CustomerReadModel;
-    customerOrderElectronicItemResponseDto?: CustomerOrderElectronicItemsReadModel[];
+    customerOrderElectronicItemResponseDto: CustomerOrderElectronicItemsReadModel[];
 }
