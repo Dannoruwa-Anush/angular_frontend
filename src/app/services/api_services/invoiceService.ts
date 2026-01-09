4acbdaaf7783d10094f2b8e19732f11e0f9b146d
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-01-09 15:19:17 +0530

    new api is added to invoice

--- src/app/services/api_services/invoiceService.ts
+++ src/app/services/api_services/invoiceService.ts
@@ -4,6 +4,8 @@ import { CrudService } from "./core_services/crud-service";
 import { HttpClient } from "@angular/common/http";
 import { SystemMessageService } from "../ui_service/systemMessageService";
 import { InvoiceReadModel } from "../../models/api_models/read_models/invoiceReadModel";
+import { catchError, map, Observable } from "rxjs";
+import { ApiResponseModel } from "../../models/api_models/core_api_models/apiResponseModel";
 
 @Injectable({
     providedIn: 'root',
@@ -34,5 +36,20 @@ export class InvoiceService extends CrudService<InvoiceReadModel, InvoiceReadMod
             customerId,
             searchKey
         });
+    }
+
+    // can add custom api methods here
+    isExistsUnpaidInvoiceByCustomerId(id: number | string): Observable<boolean> {
+        this._loading.set(true);
+
+        return this.http
+            .get<ApiResponseModel<boolean>>(`${this.baseUrl}/${this.endpoint}/customer/${id}`)
+            .pipe(
+                map(res => {
+                    this._loading.set(false);
+                    return res.data;
+                }),
+                catchError(err => this.handleHttpError(err))
+            );
     }
 }
