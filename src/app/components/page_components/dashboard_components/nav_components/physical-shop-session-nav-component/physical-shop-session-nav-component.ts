41f66e1f53d2cc74956ab9f7f46833d9bfc47a70
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-01-30 22:40:43 +0530

    ui is drafted

--- src/app/components/page_components/dashboard_components/nav_components/physical-shop-session-nav-component/physical-shop-session-nav-component.ts
+++ src/app/components/page_components/dashboard_components/nav_components/physical-shop-session-nav-component/physical-shop-session-nav-component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../../../custom_modules/material/material-module';
 
 @Component({
   selector: 'app-physical-shop-session-nav-component',
-  imports: [],
+  imports: [
+    MaterialModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './physical-shop-session-nav-component.html',
   styleUrl: './physical-shop-session-nav-component.scss',
 })
 export class PhysicalShopSessionNavComponent {
 
+  // true = active, false = inactive
+  isActive = signal(false);
+
+
+  activateSession() {
+    this.isActive.set(true);
+  }
+
+
+  deactivateSession() {
+    this.isActive.set(false);
+  }
 }
