5acdbba40f6a42624c002758b8bab82f8d7d7f8f
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-02-03 13:03:29 +0530

    order summary is modified

--- src/app/components/reusable_components/order-summary-component/order-summary-component.ts
+++ src/app/components/reusable_components/order-summary-component/order-summary-component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../../../custom_modules/material/material-module';
 import { CustomerOrderReadModel } from '../../../models/api_models/read_models/customerOrder_read_Model';
 import { OrderPaymentStatusEnum } from '../../../config/enums/orderPaymentStatusEnum';
-import { OrderStatusEnum } from '../../../config/enums/orderStatusEnum';
+import { OrderStatusEnum, getOrderStatusLabel } from '../../../config/enums/orderStatusEnum';
 import { FileService } from '../../../services/ui_service/fileService';
 
 @Component({
@@ -20,6 +20,8 @@ export class OrderSummaryComponent {
   @Input({ required: true }) order!: CustomerOrderReadModel;
 
   OrderStatusEnum = OrderStatusEnum;
+  getOrderStatusLabel = getOrderStatusLabel;
+  
   OrderPaymentStatusEnum = OrderPaymentStatusEnum;
 
   displayedColumns = [
@@ -32,5 +34,37 @@ export class OrderSummaryComponent {
 
   constructor(
     public fileService: FileService,
-  ){}
+  ) { }
+
+  // Stepper index based on order status 
+  getStepIndex(): number {
+    switch (this.order.orderStatus) {
+
+      case OrderStatusEnum.Pending:
+        return 0;
+
+      case OrderStatusEnum.Processing:
+        return 1;
+
+      case OrderStatusEnum.Shipped:
+        return 2;
+
+      case OrderStatusEnum.Delivered:
+      case OrderStatusEnum.DeliveredAfterCancellationRejected:
+        return 3;
+
+      case OrderStatusEnum.Cancel_Pending:
+        return 4;
+
+      case OrderStatusEnum.Cancelled:
+        return 5;
+
+      default:
+        return 0;
+    }
+  }
+
+  formatDate(date?: string): string {
+    return date ? new Date(date).toLocaleDateString() : '—';
+  }
 }
--- src/app/config/enums/orderStatusEnum.ts
+++ src/app/config/enums/orderStatusEnum.ts
@@ -7,3 +7,32 @@ export enum OrderStatusEnum {
     Cancelled = 6,      
     DeliveredAfterCancellationRejected = 7, 
 }
+
+//conver Enums to Human-readable labels for UI
+export function getOrderStatusLabel(status: OrderStatusEnum): string {
+  switch (status) {
+    case OrderStatusEnum.Pending:
+      return 'Pending';
+
+    case OrderStatusEnum.Processing:
+      return 'Processing';
+
+    case OrderStatusEnum.Shipped:
+      return 'Shipped';
+
+    case OrderStatusEnum.Delivered:
+      return 'Delivered';
+
+    case OrderStatusEnum.Cancel_Pending:
+      return 'Cancel Requested';
+
+    case OrderStatusEnum.Cancelled:
+      return 'Cancelled';
+
+    case OrderStatusEnum.DeliveredAfterCancellationRejected:
+      return 'Delivered (Cancel Rejected)';
+
+    default:
+      return 'Unknown';
+  }
+}
